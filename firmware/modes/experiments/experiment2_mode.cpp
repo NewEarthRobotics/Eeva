1826cb24c290c962ff51fa6bcffe1f482b5bd839
Kyle McGahee <kmcgahee@ksu.edu>
2016-05-21 22:15:10 -0500

    Added derivative filters for error on position control experiment since using dx/dt is too noisy to tune up well (credit dschin)

--- firmware/modes/experiments/experiment2_mode.cpp
+++ firmware/modes/experiments/experiment2_mode.cpp
@@ -1,5 +1,6 @@
 // Includes
 #include <cmath>
+#include "derivative_filter.h"
 #include "globs.h"
 #include "main_control_task.h"
 #include "physical_constants.h"
@@ -17,10 +18,16 @@ void MainControlTask::experiment2Mode(float experiment_input)
     // Wheel angular position control (in degrees).
     float position_command = experiment_input;
 
+    // Use derivative filters for error terms since simple dx/dt is too noisy with encoders.
+    static DerivativeFilter left_error_derivative(delta_t_, 30.0f, 0.707f);
+    static DerivativeFilter right_error_derivative(delta_t_, 30.0f, 0.707f);
+
     float left_position_error = position_command - (odometry_.left_distance / WHEEL_RADIUS * RAD2DEG);
-    motors_.left_voltage = left_position_pid.calculate(left_position_error, delta_t_);
+    float left_deriv = left_error_derivative.calculate(left_position_error);
+    motors_.left_voltage = left_position_pid.calculate(left_position_error, left_deriv, delta_t_);
 
     float right_position_error = position_command - (odometry_.right_distance / WHEEL_RADIUS * RAD2DEG);
-    motors_.right_voltage = right_position_pid.calculate(right_position_error, delta_t_);
+    float right_deriv = right_error_derivative.calculate(left_position_error);
+    motors_.right_voltage = right_position_pid.calculate(right_position_error, right_deriv, delta_t_);
 
 }
